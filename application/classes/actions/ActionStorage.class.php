42da57f4fa2622096a3c5427991cb5005d96fd12
Timofey <tim.chernih@gmail.com>
2016-08-01 12:10:13 +0300

    папочки

--- application/classes/actions/ActionStorage.class.php
+++ application/classes/actions/ActionStorage.class.php
@@ -49,6 +49,7 @@ class ActionStorage extends Action
 //        $this->AddEventPreg('/^getfile$/i', '/^[\w\-]{1,50}$/i', 'EventGetFile');
         $this->AddEventPreg('/^get/i', 'EventGetFile');
         $this->AddEventPreg('/^send/i', 'EventSendFile');
+        $this->AddEventPreg('/^tyu/i', 'EventGetByHash');
     }
 
 
@@ -84,6 +85,36 @@ class ActionStorage extends Action
         $this->SetTemplateAction('upload');
     }
 
+    protected function EventGetByHash()
+    {
+        $hash = getRequest('hash');
+        $oStortestdb = LS::E()->Storagedatabase_GetStortestdbByHash($hash);
+        $file = '.' . substr($oStortestdb->getLink(), 22);
+        // сбрасываем буфер вывода PHP, чтобы избежать переполнения памяти выделенной под скрипт
+        // если этого не сделать файл будет читаться в память полностью!
+        if (ob_get_level()) {
+            ob_end_clean();
+        }
+        // заставляем браузер показать окно сохранения файла
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($file));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        // читаем файл и отправляем его пользователю
+        readfile($file);
+
+        echo "crfxfk";
+
+        /**
+         * Устанавливаем шаблон вывода
+         */
+        $this->SetTemplate(false);
+    }
+
     # Функция получания файла по ссылкке /storage/sendfile/
     protected function EventSendFile()
     {
@@ -101,16 +132,18 @@ class ActionStorage extends Action
                 $enddir = './storage/';
                 $dirs = explode("/", $address);
                 foreach ($dirs as $dir) {
+                    error_reporting(0);
                     mkdir($enddir . $dir . '/');
+                    error_reporting(E_ERROR | E_WARNING | E_PARSE);
                     $enddir .= $dir . '/';
                 }
-                $uploadfile = $enddir . basename($_FILES['file']['name']);
-                echo print_r($uploadfile);
+                $uploadfile = $enddir .= basename($_FILES['file']['name']);
             }
 
             copy($_FILES['file']['tmp_name'], $uploadfile);
+
             // генерируем ссылку на скачивание
-            $download = 'http://iu8-praktika.ru/storage/';
+            $download = 'http://iu8-praktika.ru/tyu/?hash=' .$hash;
             $File_upload = is_uploaded_file($_FILES['file']['tmp_name']);
 
             // передача переменных в шаблон
@@ -126,6 +159,7 @@ class ActionStorage extends Action
             $oStortestdb->setHash($hash);
             $oStortestdb->setLink($download);
             $oStortestdb->setDesc($description);
+            $oStortestdb->setDir(substr($uploadfile, 1));
             $oStortestdb->Save();
 
             $this->Viewer_Assign('iddownload', $download);
